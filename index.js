2736a9de805a04439e139eee84433c74dc396ba1
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-20 18:24:57 -0300

    feat/ main app file

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 // [START app]
-import express from 'express';
-
-const app = express();
-
-// [START enable_parser]
-// This middleware is available in Express v4.16.0 onwards
-app.use(express.json({ extended: true }));
-// [END enable_parser]
-
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
+import { app } from './utils/router.js';
 
 app.get('/', (req, res) => {
   res.send('Hi! :)');
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+// [END app]
