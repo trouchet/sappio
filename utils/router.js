27c3a1338c7fb424858894af1e1c6459cba5d0cd
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-21 18:34:46 -0300

    refac/ keep-alive logging

--- utils/router.js
+++ utils/router.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import helmet from 'helmet';
 
-import { 
+import * as dotenv from 'dotenv';
+
+import {
   morganMiddleware,
   agentMorganReporter,
   log_message,
 } from 'quivero-api/utils/logging/logger.js';
 
-import helmet from "helmet";
+import { Logtail } from '@logtail/node';
+import { LogtailTransport } from '@logtail/winston';
+
+dotenv.config();
+
+// Use winston agent to report for Logtail
+if (process.env.LOGTAIL_TOKEN) {
+  const logtail = new Logtail(process.env.LOGTAIL_TOKEN);
+
+  agentMorganReporter.add(
+    new LogtailTransport(logtail),
+  );
+}
 
 export const app = express();
 
@@ -30,7 +45,9 @@ app.use(helmet());
 // Listen to the App Engine-specified port, or 8080 otherwise
 const APP_PORT = process.env.PORT || 8080;
 
-app.listen(APP_PORT, () => {
-  log_message(agentMorganReporter, 'info', `Listening on port: ${APP_PORT}`);
-});
-
+app.listen(
+  APP_PORT,
+  () => {
+    log_message(agentMorganReporter, 'info', `Listening on port: ${APP_PORT}`);
+  },
+);
