c1714a08d7313fea5a7e89edc9b899cba0ae03df
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-26 17:31:57 -0300

    feat/ time tools

---
+++ utils/time.js
@@ -0,0 +1,32 @@
+import { logger } from './logger.js'
+import { log_message, } from 'quivero-api/utils/logging/logger.js';
+
+const timeHierarchy = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year']
+const timeScaler = [60, 60, 24, 7, 30, 12]
+
+const log = (type, msg) => log_message(logger, type, msg);
+
+export const convertTimeDouble = (value, from, to) => {
+	const fromIndex = timeHierarchy.indexOf(from);
+	const toIndex = timeHierarchy.indexOf(to);
+	const errorMsg = "Item _ITEM_ must belong to list ["+timeHierarchy+"]";
+
+	if(fromIndex === -1) {
+		log('error', errorMsg.replace('_ITEM_', from));
+	}
+
+	if(toIndex === -1) {
+		log('error', errorMsg.replace('_ITEM_', to));
+	} 
+	
+	let reduce_fun = [];
+	if(fromIndex >= toIndex) {
+		reduce_fun = (a, b) => a*b;
+	} else {
+		reduce_fun = (a, b) => a/b;
+	}
+
+	return timeScaler.slice(fromIndex, toIndex).reduce(
+		reduce_fun, value
+	)
+}
