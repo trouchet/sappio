9314a92dcca0abf439b6d01b81f7cbb1b4db1d03
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-26 17:31:29 -0300

    refac/ reorganize files and add rate limiter

--- utils/router.js
+++ utils/router.js
@@ -1,21 +1,27 @@
 import express from 'express';
 import helmet from 'helmet';
+import favicon from 'serve-favicon'
+import path from 'path'
+import { rateLimit } from "express-rate-limit";
+import title from 'express-title';
 
 import { Logtail } from '@logtail/node';
 import { LogtailTransport } from '@logtail/winston';
+import { convertTimeDouble } from './time.js';
 
-import {
-  morganMiddleware,
-  log_message,
-} from 'quivero-api/utils/logging/logger.js';
+import { morganMiddleware, log_message, } from 'quivero-api/utils/logging/logger.js';
+import { errorsMiddleware } from './middlewares.js';
+import { logger } from './logger.js';
 
-import {
-  errorsMiddleware
-} from './middlewares.js';
+const obsvWindow_min = 1;
+const maxRequestCount = 5;
 
-import {
-  logger
-} from './logger.js';
+const limiter = rateLimit({
+    windowMs: convertTimeDouble(obsvWindow_min, 'minute', 'second')*1000  , // 15 minutes
+    max: maxRequestCount, // limit each IP to 100 requests per windowMs
+    message: "Too many accounts created from this IP, please try again after "+obsvWindow_min+" minutes"
+
+  });
 
 export const app = express();
 
@@ -35,6 +41,22 @@ app.use(morganMiddleware);
 // [START logger]
 app.use(helmet());
 // [END logger]
+
+// [START logger]
+app.use(limiter); //  apply to all requests
+// [END logger]
+
+// [START favicon]
+app.use(
+  favicon(
+    process.cwd()+"/"+"public"+"/"+"favicon.ico"
+  )
+)
+// [END favicon]
+
+// [START title]
+app.set('title', 'Sappio');
+// [END title]
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const APP_PORT = process.env.APP_PORT || 8080;
