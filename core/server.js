85b633b7a011e06d9ff7e2ce6056b3c5cb4bfeb2
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-27 21:55:38 -0300

    refac/ server file with middleware and router use

--- core/server.js
+++ core/server.js
@@ -1,13 +1,24 @@
-import { log } from '../utils/logger.js';
-import { router } from './routes.js';
 import express from 'express';
+
+import { log } from '../utils/logger.js';
+import { router } from './routes/root.js';
+
 import swaggerUi from 'swagger-ui-express'
 import { swaggerSpec } from '../utils/swagger.js'
+import { middlewares } from './middlewares/bundler.js'
 
-export const app = express();
+const app = express();
+
+// Swagger middleware
+router.use(
+  '/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec)
+);
+
+for(const middleware of middlewares) {
+    app.use(middleware)
+}
 
 app.use(router);
-app.use('/docs-api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Listen to the App Engine-specified port, otherwise 8080
 const APP_PORT = process.env.APP_PORT || 8080;
@@ -18,3 +29,5 @@ app.listen(
     log('info', `Listening on port: ${APP_PORT}`);
   },
 );
+
+export default app;
