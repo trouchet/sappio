1a556e8fa753f02ce38a55223c744cc39cf196fb
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-27 21:29:59 -0300

    refac/ all routes defined on one folder

---
+++ core/routes/root.js
@@ -0,0 +1,88 @@
+import express from 'express';
+
+export const router = express.Router();
+
+import { getToken } from '../controllers/token.js';
+import { healthCheck } from '../controllers/healthcheck.js';
+import { routerRoutes } from '../../utils/router.js';
+
+/**
+ * @openapi
+ * /*:
+ *   get:
+ *     description: Default page in case 
+ *     responses:
+ *       200:
+ *         description: Returns a smile face.
+ */
+router.get(
+  '/all', 
+  function (req, res) {
+    res.send(routerRoutes(router));
+  }
+)
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to sappio, your simple app!
+ *     responses:
+ *       200:
+ *         description: Returns a smile face.
+ */
+router.get(
+  '/', 
+  (req, res) => {
+    res.send(':)');
+  }
+);
+
+/**
+ * @openapi
+ * /token:
+ *   get:
+ *     description: Generates a valid JWT token  
+ *     responses:
+ *       200:
+ *         description: Returns a smile face.
+ */
+router.get(
+  '/token', 
+  getToken
+);
+
+/**
+ * @openapi
+ * /healthcheck:
+ *   get:
+ *     description: Generates a health object   
+ *     responses:
+ *       200:
+ *         description: Returns an object a message and package version 
+ */
+router.get(
+  '/healthcheck', 
+  healthCheck
+);
+
+/**
+ * import { Validator } from "express-json-validator-middleware";
+ * const { validate } = new Validator();
+
+/**
+ * The `validate` method accepts an object which maps request
+ * properties to the JSON schema you want them to be validated
+ * against e.g.
+ *
+ * { requestPropertyToValidate: jsonSchemaObject }
+ *
+ * Validate `request.body` against `addressSchema`.
+ */
+/**
+app.post("/address", validate({ body: addressSchema }), (request, response) => {
+  // Route handler logic to run when `request.body` has been validated.
+   
+  response.send({});
+});
+**/
