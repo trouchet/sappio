27f4b1009f97df2ee4b196142076a908841cf40e
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-28 22:47:40 -0300

    refac/ provide clear logs for debugging

--- utils/logger.js
+++ utils/logger.js
@@ -5,13 +5,9 @@ import json from 'morgan-json';
 import { Logtail } from '@logtail/node';
 import { LogtailTransport } from '@logtail/winston';
 
-import * as dotenv from 'dotenv';
+import { env } from '../config/dotenv.js'
 
-dotenv.config(
-  {
-    path: process.cwd()+'/config/.env'
-  }
-);
+import { getTimestampTimezone } from './date.js' 
 
 const {
   createLogger,
@@ -44,12 +40,20 @@ const {
  * @param {String} label_msg
  */
 export const logging = (label_msg = 'default') => {
+  const log_info_parser = (info) => {
+    const time_prefix = `[${info.timestamp} - ${label_msg}]`;
+    const info_suffix = `${info.level}: ${info.message}`;
+
+    return `${time_prefix} - ${info_suffix}`
+  } 
+
+
   const logger_setup = {
     format: format.combine(
       label({ label: label_msg }),
-      format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss.sss A' }),
+      format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss.ss' }),
       format.colorize(),
-      format.printf((info) => `[${info.timestamp} - ${label_msg}] ${info.level}: ${info.message}`),
+      format.printf(log_info_parser),
     ), transports: [
       new transports.Console()
     ],
@@ -77,14 +81,16 @@ export const log_message = (logger__, level, message) => {
 export const agentMorganReporter = logging('morgan');
 
 // Use winston agent to report for Logtail
-if (process.env.LOGTAIL_TOKEN) {
-  const logtail = new Logtail(process.env.LOGTAIL_TOKEN);
+if (env.LOGTAIL_TOKEN) {
+  const logtail = new Logtail(env.LOGTAIL_TOKEN);
 
   agentMorganReporter.add(new LogtailTransport(logtail));
 }
 
+morgan.token('type', function (req, res) { return req.headers['content-type'] })
+
 const morgan_format = json(
-  ':method :url :status :res[content-length] bytes :response-time ms'
+  ':type :method :status :url :res[content-length] bytes :total-time ms :response-time ms'
 );
 
 const stream_channels  = {
@@ -98,7 +104,9 @@ const stream_channels  = {
  * @abstract Morgan middleware to log app access
  *
  */
-export const morganMiddleware = morgan(morgan_format, stream_channels);
+export const morganMiddleware = morgan(
+    morgan_format, 
+    stream_channels);
 
 export const logger = agentMorganReporter
 export const log = (type, msg) => log_message(logger, type, msg);
