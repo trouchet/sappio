f4a8721dea4ce39d4b84e1557cf480b932063d6f
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-28 22:24:28 -0300

    feat/ error handling

--- core/middlewares/errors.js
+++ core/middlewares/errors.js
@@ -1,4 +1,12 @@
 import { log } from '../../utils/logger.js';
+import { env } from '../../config/dotenv.js';
+import { 
+  getErrorMessage, 
+  getHttpStatusCode,
+  logErrorMessage
+} from '../../utils/error-handler.js'
+
+const NODE_ENVIRONMENT = env.NODE_ENV || "test";
 
 // Error handling Middleware functions
 const errorLogger = (error, req, res, next) => {
@@ -9,14 +17,102 @@ const errorLogger = (error, req, res, next) => {
 }
 
 const errorResponder = (error, req, res, next) => {
+  log('error', error.message)
+
   res.header("Content-Type", 'application/json')
   
-  const status = error.status || 400 || 500
-  response.status(status).send(error.message)
-  log('error', error.message)
+  const status = error.status || 500
+  
+  res.status(status).send(error.message)
+}
+
+/**
+ * Generic Express error handler middleware.
+ *
+ * @param {Error} error - An Error object.
+ * @param {Object} request - Express request object
+ * @param {Object} response - Express response object
+ * @param {Function} next - Express `next()` function
+ */
+const errorHandlerMiddleware = (error, request, response, next) => {
+  const errorMessage = getErrorMessage(error);
+
+  logErrorMessage(errorMessage);
+
+  /**
+   * If response headers have already been sent,
+   * delegate to the default Express error handler.
+   */
+  if (response.headersSent) {
+    return next(error);
+  }
+
+  const errorResponse = {
+    statusCode: getHttpStatusCode({ error, response }),
+    body: undefined
+  };
+
+  /**
+   * Error messages and error stacks often reveal details
+   * about the internals of your application, potentially
+   * making it vulnerable to attack, so these parts of an
+   * Error object should never be sent in a response when
+   * your application is running in production.
+   */
+  if (NODE_ENVIRONMENT !== "production") {
+    errorResponse.body = errorMessage;
+  }
+
+  /**
+   * Set the response status code.
+   */
+  response.status(errorResponse.statusCode);
+
+  /**
+   * Send an appropriately formatted response.
+   *
+   * The Express `res.format()` method automatically
+   * sets `Content-Type` and `Vary: Accept` response headers.
+   *
+   * @see https://expressjs.com/en/api.html#res.format
+   *
+   * This method performs content negotation.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Content_negotiation
+   */
+  response.format({
+    //
+    // Callback to run when `Accept` header contains either
+    // `application/json` or `*/*`, or if it isn't set at all.
+    //
+    "application/json": () => {
+      /**
+       * Set a JSON formatted response body.
+       * Response header: `Content-Type: `application/json`
+       */
+      response.json({ message: errorResponse.body });
+    },
+    /**
+     * Callback to run when none of the others are matched.
+     */
+    default: () => {
+      /**
+       * Set a plain text response body.
+       * Response header: `Content-Type: text/plain`
+       */
+      response.type("text/plain").send(errorResponse.body);
+    },
+  });
+
+  /**
+   * Ensure any remaining middleware are run.
+   */
+  next();
 }
 
 export const errors_MWs = [
+    errorHandlerMiddleware,
     errorLogger,
     errorResponder
   ]; 
+
---
+++ utils/error-handler.js
@@ -0,0 +1,90 @@
+import { log } from './logger.js'
+
+/**
+ * Extract an error stack or error message from an Error object.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+ *
+ * @param {Error} error
+ * @return {string} - String representation of the error object.
+ */
+export const getErrorMessage = (error) => {
+	/**
+	 * If it exists, prefer the error stack as it usually
+	 * contains the most detail about an error:
+	 * an error message and a function call stack.
+	 */
+	if (error.stack) {
+		return error.stack;
+	}
+
+	if (typeof error.toString === "function") {
+		return error.toString();
+	}
+
+	return "";
+}
+
+/**
+ * Log an error message to stderr.
+ *
+ * @see https://nodejs.org/dist/latest-v14.x/docs/api/console.html#console_console_error_data_args
+ *
+ * @param {string} error
+ */
+export const logErrorMessage = (error) => {
+	log('error', error);
+}
+
+/**
+ * Determines if an HTTP status code falls in the 4xx or 5xx error ranges.
+ *
+ * @param {number} statusCode - HTTP status code
+ * @return {boolean}
+ */
+export const isErrorStatusCode = (statusCode) => {
+	return statusCode >= 400 && statusCode < 600;
+}
+
+/**
+ * Look for an error HTTP status code (in order of preference):
+ *
+ * - Error object (`status` or `statusCode`)
+ * - Express response object (`statusCode`)
+ *
+ * Falls back to a 500 (Internal Server Error) HTTP status code.
+ *
+ * @param {Object} options
+ * @param {Error} options.error
+ * @param {Object} options.response - Express response object
+ * @return {number} - HTTP status code
+ */
+export const getHttpStatusCode = ({ error, response }) => {
+	/**
+	 * Check if the error object specifies an HTTP
+	 * status code which we can use.
+	 */
+	const statusCodeFromError = error.status || error.statusCode;
+	if (isErrorStatusCode(statusCodeFromError)) {
+		return statusCodeFromError;
+	}
+
+	/**
+	 * The existing response `statusCode`. This is 200 (OK)
+	 * by default in Express, but a route handler or
+	 * middleware might already have set an error HTTP
+	 * status code (4xx or 5xx).
+	 */
+	const statusCodeFromResponse = response.statusCode;
+	if (isErrorStatusCode(statusCodeFromResponse)) {
+		return statusCodeFromResponse;
+	}
+
+	/**
+	 * Fall back to a generic error HTTP status code.
+	 * 500 (Internal Server Error).
+	 *
+	 * @see https://httpstatuses.com/500
+	 */
+	return 500;
+}
