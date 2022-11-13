e724101b2ed02ff4c9b96ad3e58dd2bed57b466f
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-13 19:49:51 -0300

    feat/ validation function

---
+++ utils/validation.js
@@ -0,0 +1,11 @@
+import { body, validationResult } from "express-validator";
+
+// Finds the validation errors in this request and wraps them in an object with handy functions
+export const validateRequest = (req, invalid_code) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(invalid_code).json({
+      errors: errors.array(),
+    });
+  }
+};
