a45dde98effbd5b2e944e008463dfc2ff5322858
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-14 10:48:36 -0300

    refac/ avoid morgan logger in a high load app

--- core/middlewares/logging.js
+++ core/middlewares/logging.js
@@ -1,14 +1,7 @@
-import { morganMiddleware, log } from "../../utils/logger.js";
+import { morganMiddleware } from "../../utils/logger.js";
 import actuator from "express-actuator";
 
-import { require } from "../../utils/commonjs.js";
-import { env } from "../../config/dotenv.js";
-
-const status_monitor = require("express-status-monitor");
-
-
 export const logging_MWs = [
-  morganMiddleware,
-  status_monitor(),
+  //morganMiddleware,
   actuator(),
 ];
