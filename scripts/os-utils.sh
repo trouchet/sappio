91e6a23e2e5667988ab3aee20ae378e1779601e5
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-14 12:10:08 -0300

    feat/ identify operational system tool

--- scripts/os-utils.sh
+++ scripts/os-utils.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -e
+
+# strip "v" prefix if present
+VERSION="${VERSION#v}"
+
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
@@ -51,16 +56,9 @@ version_gte() {
 	eval calver_compare "$VERSION" "$1"
 }
 
-is_darwin() {
-	case "$(uname -s)" in
-	*darwin* ) true ;;
-	*Darwin* ) true ;;
-	* ) false;;
-	esac
-}
-
 get_distribution() {
 	lsb_dist=""
+
 	# Every system that we officially support has /etc/os-release
 	if [ -r /etc/os-release ]; then
 		lsb_dist="$(. /etc/os-release && echo "$ID")"
@@ -72,6 +70,61 @@ get_distribution() {
 	lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 
 	echo "$lsb_dist"
+}
+
+# Check if this is a forked Linux distro
+check_forked() {
+
+	# Check for lsb_release command existence, it usually exists in forked distros
+	if command_exists lsb_release; then
+		# Check if the `-u` option is supported
+		set +e
+		lsb_release -a -u > /dev/null 2>&1
+		lsb_release_exit_code=$?
+		set -e
+
+		# Check if the command has exited successfully, it means we're in a forked distro
+		if [ "$lsb_release_exit_code" = "0" ]; then
+			# Print info about current distro
+			cat <<-EOF
+			You're using '$lsb_dist' version '$dist_version'.
+			EOF
+
+			# Get the upstream release info
+			lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[:space:]')
+			dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[:space:]')
+
+			# Print info about upstream distro
+			cat <<-EOF
+			Upstream release is '$lsb_dist' version '$dist_version'.
+			EOF
+		else
+			if [ -r /etc/debian_version ] && [ "$lsb_dist" != "ubuntu" ] && [ "$lsb_dist" != "raspbian" ]; then
+				if [ "$lsb_dist" = "osmc" ]; then
+					# OSMC runs Raspbian
+					lsb_dist=raspbian
+				else
+					# We're Debian and don't even know it!
+					lsb_dist=debian
+				fi
+				dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+				case "$dist_version" in
+					11)
+						dist_version="bullseye"
+					;;
+					10)
+						dist_version="buster"
+					;;
+					9)
+						dist_version="stretch"
+					;;
+					8)
+						dist_version="jessie"
+					;;
+				esac
+			fi
+		fi
+	fi
 }
 
 dist_deprecation_notice() {
@@ -94,51 +147,58 @@ get_dist_version() {
 
 	lsb_dist=$( get_distribution )
 	case "$lsb_dist" in
+		ubuntu)
+			if command_exists lsb_release; then
+				dist_version="$(lsb_release --codename | cut -f2)"
+			fi
+			if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
+				dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
+			fi
+		;;
 
-	ubuntu)
-		if command_exists lsb_release; then
-			dist_version="$(lsb_release --codename | cut -f2)"
-		fi
-		if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
-			dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
-		fi
-	;;
+		debian|raspbian)
+			dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
+			case "$dist_version" in
+				11)
+					dist_version="bullseye"
+				;;
+				10)
+					dist_version="buster"
+				;;
+				9)
+					dist_version="stretch"
+				;;
+				8)
+					dist_version="jessie"
+				;;
+			esac
+		;;
 
-	debian|raspbian)
-		dist_version="$(sed 's/\/.*//' /etc/debian_version | sed 's/\..*//')"
-		case "$dist_version" in
-			11)
-				dist_version="bullseye"
-			;;
-			10)
-				dist_version="buster"
-			;;
-			9)
-				dist_version="stretch"
-			;;
-			8)
-				dist_version="jessie"
-			;;
-		esac
-	;;
+		centos|rhel|sles)
+			if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
+				dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
+			fi
+		;;
 
-	centos|rhel|sles)
-		if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
-			dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
-		fi
-	;;
-
-	*)
-		if command_exists lsb_release; then
-			dist_version="$(lsb_release --release | cut -f2)"
-		fi
-		if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
-			dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
-		fi
-	;;
+		*)
+			if command_exists lsb_release; then
+				dist_version="$(lsb_release --release | cut -f2)"
+			fi
+			if [ -z "$dist_version" ] && [ -r /etc/os-release ]; then
+				dist_version="$(. /etc/os-release && echo "$VERSION_ID")"
+			fi
+		;;
 	esac
 
 	echo $dist_version
+}
+
+is_darwin() {
+	case "$(uname -s)" in
+	*darwin* ) true ;;
+	*Darwin* ) true ;;
+	* ) false;;
+	esac
 }
 
 is_wsl() {
@@ -173,3 +233,36 @@ check_dist_deprecation() {
 			;;
 	esac
 }
+
+
+lsb_dist=$( get_distribution )
+dist_version=$( get_dist_version )
+
+if is_wsl; then
+	echo
+	echo "Operating system WSL DETECTED."
+	echo
+	exit 1
+	cat >&2 <<-'EOF'
+
+	EOF
+	( set -x; sleep 20 )
+fi
+
+if [ -z "$lsb_dist" ]; then
+	if is_darwin; then
+		echo
+		echo "Operating system 'macOS' DETECTED."
+		echo
+		exit 1
+		cat >&2 <<-'EOF'
+
+		EOF
+		( set -x; sleep 20 )
+	fi
+fi
+
+check_forked
+check_dist_deprecation
+
+echo "Operating system $lsb_dist, distribution $dist_version DETECTED."
