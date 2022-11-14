954c896cacb1fb9a4e26fc6b43a0f3ade37a6196
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-13 22:08:57 -0300

    feat/ add /all route based on app

--- core/server.js
+++ core/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import swStats from "swagger-stats";
 
 import { log } from "../utils/logger.js";
 import { router } from "./routes/root.js";
@@ -12,9 +13,17 @@ const app = express();
 
 app.set("views", process.cwd() + "/core/views");
 app.set("view engine", "pug");
+
 app.use(express.static(process.cwd() + "/public"));
 
 // Swagger middleware
+app.use(
+  swStats.getMiddleware(
+    {
+      swaggerSpec: swaggerSpec
+    }
+  ));
+
 router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 for (const middleware of middlewares) {
@@ -29,6 +38,19 @@ app.use(router);
 for (const error_middleware of errors_MWs) {
   app.use(error_middleware);
 }
+
+/**
+ * @openapi
+ * /all:
+ *   get:
+ *     description: available routes
+ *     responses:
+ *       200:
+ *         description: Returns the available routes.
+ */
+app.get("/all", function (req, res) {
+  res.send(parseExpressApp(app));
+});
 
 // Listen to the App Engine-specified port, otherwise 8080
 const APP_PORT = process.env.APP_PORT || 8080;
