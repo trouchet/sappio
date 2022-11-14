4f88dfd6fe7af8a236e9e06edcbda20d62d36a91
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-13 22:01:22 -0300

    feat/ healthcheck

--- core/controllers/healthcheck.js
+++ core/controllers/healthcheck.js
@@ -4,9 +4,17 @@ import { log } from "../../utils/logger.js";
 export const healthCheck = async (req, res, next) => {
   log("debug", "healthCheck controller called");
 
-  res.status = 200;
-  res.send({
-    message: "Sappio server running",
-    version: pkg.version,
-  });
+  const healthcheck = {
+      uptime: process.uptime(),
+      message: 'Sappio server running',
+      timestamp: Date.now(),
+      version: pkg.version,
+  };
+  
+  try {
+      res.send(healthcheck);
+  } catch (error) {
+      healthcheck.message = error;
+      res.status(503).send();
+  }
 };
