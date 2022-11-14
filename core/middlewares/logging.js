436177c5a8f25f9860d9a87dee04e3230966100d
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-13 22:02:02 -0300

    refac/ remove pino dependency

--- core/middlewares/logging.js
+++ core/middlewares/logging.js
@@ -1,26 +1,14 @@
 import { morganMiddleware, log } from "../../utils/logger.js";
 import actuator from "express-actuator";
-import pino from "express-pino-logger";
 
 import { require } from "../../utils/commonjs.js";
 import { env } from "../../config/dotenv.js";
 
 const status_monitor = require("express-status-monitor");
 
-const status_options = {
-  healthChecks: [
-    {
-      protocol: "http",
-      host: env.APP_HOST | "localhost",
-      path: "/healthcheck",
-      port: env.APP_PORT,
-    },
-  ],
-};
 
 export const logging_MWs = [
   morganMiddleware,
-  status_monitor(status_options),
+  status_monitor(),
   actuator(),
-  pino(),
 ];
