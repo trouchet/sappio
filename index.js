7e2c850f4561e95d2da048075053f39065c63c19
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-20 10:21:46 -0300

    config/ package*

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 // [START app]
-import app from "./core/app.js";
+import process from 'node:process';
 
+import app from "./core/app.js";
 import { log } from "./utils/logger.js";
 import { env } from "./config/dotenv.js";
+import db from "./db/knex.js";
 
 // Listen to the App Engine-specified port, otherwise 3000
-const NUM_APPS = parseInt(env.NUM_PORTS) || 1;
-let curr_port = parseInt(env.APP_INIT_PORT) || 3000;
+const curr_port = parseInt(env.APP_INIT_PORT) || 3000;
 
-for(let i = 0; i < NUM_APPS; i++) {	
-	app.listen(
-		curr_port+i , 
-		(err) => {
-			err ?
-			log("error", `Failed to listen on PORT ${curr_port+i}`):
-			log("info", `Application server listening on PORT ${curr_port+i}`)
-		}
-	);
-}
+app.listen(
+	curr_port, 
+	(err) => {
+		err ?
+		log("error", `Failed to listen on PORT ${curr_port}`):
+		log("info", `Application server listening on PORT ${curr_port}`)
+	
+	  process.send('ready')
+	}
+);
+
+process.on(
+	'message', 
+	function(msg) {
+	  if (msg == 'shutdown') {
+	    log('info', 'Closing all connections...')
+	    
+	    setTimeout(
+	    	function() {
+	      		log('Finished closing connections')
+	      		process.exit(0)
+	    	}, 
+	    	1500
+	    )
+	  }
+})
 
