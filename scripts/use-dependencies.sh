39e31cc71a7d994be966671e1726b80a4b4d2bcd
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-04 12:18:40 -0300

    refac/ add grep line number

--- scripts/use-dependencies.sh
+++ scripts/use-dependencies.sh
@@ -19,7 +19,7 @@ grepignore_tokens=(
         'node_modules' 
         '.git' 
         'package-lock'
-        'package'
+        'codecov'
     )
 
 touch "$grepignore_fname"
@@ -28,14 +28,18 @@ for grepignore_token in "${grepignore_tokens[@]}"; do
 done
 
 dependency_grep=''
+dependency_count=0
 
 for key in "${keys[@]}"; do
     jsonKeys "$(jsonValue "$(cat $1)" "$key")" | \
     while read dependency; do
-        dependency_grep="$(grep -rnw . -e "$dependency" | grep -f '.grepignore' -v)"
+        grep_command="grep -rnw . -e "$dependency" | \
+                      grep -v -f <(printf '%s\n' "${grepignore_tokens[@]}")"
+        dependency_grep="$(eval "$grep_command")"
+        dependency_count="$(eval "$grep_command" | wc -l)"
         
         echo "------------------------------------------------"
-        echo "Dependency: $dependency"
+        echo "Dependency \"$dependency\": count $dependency_count"
         echo "$dependency_grep"
         echo "------------------------------------------------"
     done
