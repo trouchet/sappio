14d78fc36e1835c9e8f16e8b43744e16cdc8414b
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-04 12:00:00 -0300

    feat/ used dependencies on project

---
+++ scripts/use-dependencies.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function jsonValue() {
+    echo "$1" | \
+    jq -r ".$2"
+}
+
+function jsonKeys() {
+    echo "$1" | \
+    jq 'keys' | \
+    perl -lne 'print $2 while m{(["'\''])((?:\\.|(?!\1).)*+)\1}g'
+}
+
+keys=( 'dependencies' 'devDependencies' )
+grepignore_fname='.grepignore'
+rm "$grepignore_fname"
+
+grepignore_tokens=( 
+        'node_modules' 
+        '.git' 
+        'package-lock'
+        'package'
+    )
+
+touch "$grepignore_fname"
+for grepignore_token in "${grepignore_tokens[@]}"; do
+    echo "$grepignore_token" >> "$grepignore_fname"
+done
+
+dependency_grep=''
+
+for key in "${keys[@]}"; do
+    jsonKeys "$(jsonValue "$(cat $1)" "$key")" | \
+    while read dependency; do
+        dependency_grep="$(grep -rnw . -e "$dependency" | grep -f '.grepignore' -v)"
+        
+        echo "------------------------------------------------"
+        echo "Dependency: $dependency"
+        echo "$dependency_grep"
+        echo "------------------------------------------------"
+    done
+done
+
+rm "$grepignore_fname"
