54c428b65d2c962b4aed6f796a74d0c0df6182f6
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-07 17:23:35 -0300

    refac/ add more useful docker utils

--- scripts/docker-clean.sh
+++ scripts/docker-clean.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-function sanitize() {
-  docker system prune --volumes -f
-
-  for image_id in $(docker images --filter "dangling=true" -q --no-trunc); do
-    docker rmi $image_id 
-  done
-}
+BASEDIR=$(dirname $0)
+source "${BASEDIR}/docker-utils.sh"
 
 echo "Removing dangling or stopped containers, networks and volumes"
 sanitize
@@ -23,7 +18,7 @@ elif docker ps -a | grep "$1"; then
 		echo "Container image $IMAGE_NAME with ID $container_id exists!"
 		echo "We will stop and remove from active images."
 
-		docker stop $container_id && docker rm $container_id
+		stopAndRemoveContainer $container_id
 	else
 		echo "We found more than one container with name $IMAGE_NAME."
 		echo $(docker ps -a | grep $1)
--- scripts/docker-utils.sh
+++ scripts/docker-utils.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Remove dangling and stopped docker content
+#
+# examples:
+# 	>> sanitize # 0 (success)
+# 	Deleted Containers:
+# 	container_xpto
+#
+# 	Deleted Networks:
+# 	network_xpto
+#
+# 	Deleted Volumes:
+# 	volume_xpto
+#
+#   Total reclaimed space: 0B
 sanitize() {
   docker system prune --volumes -f
 
@@ -8,7 +22,24 @@ sanitize() {
   done
 }
 
+# List containers with certain text snippet on containername
+#
+# examples:
+# 	>> listContainerPIDsByToken container_name_snippet
+#   container_id_1
+#   container_id_2
 listContainerPIDsByToken() {
     docker ps -a | grep $1 | awk '{ print $1 }'
 }
- 
+
+# stopAndRemoveContainer
+#
+# examples:
+# 	>> stopAndRemoveContainer container_id # 0/1 (success/fail)
+stopAndRemoveContainer() {
+    docker stop $1 && docker rm $1
+}
+
+export -f sanitize
+export -f listContainerPIDsByToken
+export -f stopAndRemoveContainer
