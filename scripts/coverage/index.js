42a1b95889aed5a1606ccc29a556df5f147cd7c2
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-07 15:17:14 -0300

    docs/ bats coverage

---
+++ scripts/coverage/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+{"link":"bats.57fcaccf5549d65a/index.html","title":"bats","summary_name":"bats","covered_class":"lineNoCov","covered":"0.0","covered_lines":"0","uncovered_lines":"0","total_lines" : "0"},
+], merged_files:[]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "bats", "date" : "2022-12-07 15:12:09", "instrumented" : 0, "covered" : 0,};
