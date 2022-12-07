9ec3292d3a36f63488d463ef0efe81083239300c
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-07 15:16:24 -0300

    fet/ bats coverage

--- scripts/bats-coverage.sh
+++ scripts/bats-coverage.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# Get keys from json dictionary
+# 
+# examples:
+# 	>> bats-coverage . ./coverage/ ./tests
+#   a
+#   b
 
 rm -rf $2
 
-kcov --report-only \
-     --clean \
+kcov --clean \
      --bash-dont-parse-binary-dir \
      --include-pattern=.sh \
      --exclude-pattern=tests \
      --include-path=$1 $2 bats $3
-
