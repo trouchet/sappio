007073f3f66a6441e1f5762033d8e58f8e828812
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-07 17:20:03 -0300

    refac/ working build scripts

--- scripts/node-build.sh
+++ scripts/node-build.sh
@@ -3,25 +3,33 @@
 BASEDIR=$(dirname $0)
 source "${BASEDIR}/linux-utils.sh"
 
+NODE_VERSION='16'
+NVM_VERSION='18.12.1'
+
+# Verifies if command runs as sudo
+sh_c=$( get_if_root )
+
 os_install_repo="$(echo "$( os_info )" | awk '{ print $3 }' FS=":")"
 
 # Install curl if not already present
-$os_install_repo install curl -y 
+$sh_c "$os_install_repo install curl -y" 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
 # Retrieve necessary repositories
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
-
-export NVM_DIR="$HOME/.nvm"
+$sh_c "curl curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash"
+$sh_c "export NVM_DIR="$HOME/.nvm""
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+source ~/.bashrc
+
 # Install node
-curl https://deb.nodesource.com/setup_16.x | sudo bash -
-$os_install_repo install -y nodejs
+$sh_c "curl https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo bash - "
+$sh_c "$os_install_repo install -y nodejs"
+$sh_c "$os_install_repo install -y gcc g++ make"
 
 # Install nvm
-nvm install lts/*
+nvm install "18.12.1" --reinstall-packages-from=current -y --latest-npm
 
 echo "=================================================================="
 
