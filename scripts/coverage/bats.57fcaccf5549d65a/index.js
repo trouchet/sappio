7e6c9c7f70f61453831edc8c862b51dbda224f88
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-08 17:49:19 -0300

    refac/ remove scripts/coverage

--- scripts/coverage/bats.57fcaccf5549d65a/index.js
+++
@@ -1,5 +0,0 @@
-var data = {files:[
-]};
-var percent_low = 25;var percent_high = 75;
-var header = { "command" : "bats", "date" : "2022-12-07 15:12:09", "instrumented" : 0, "covered" : 0,};
-var merged_data = [];
