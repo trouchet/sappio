2e2caefb049fef06e9fb5ec7df8618fdb1472d00
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-15 17:10:37 -0300

    refac/ quotes and javascript extension on import

--- src/core/utils/validation.js
+++ src/core/utils/validation.js
@@ -1,7 +1,9 @@
 import { body, validationResult } from 'express-validator';
 
 // Finds the validation errors in this request and wraps them in an object with handy functions
-export const validateRequest = (req, invalid_code) => {
+export let validateRequest = () => {};
+
+export default validateRequest = (req, invalid_code) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(invalid_code).json({
