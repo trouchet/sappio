a162d1a060eda6f0f5bcc918d72cc1d1c57a88cf
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-30 20:41:30 -0300

    refac/ double quotes -> single quotes

--- src/config/jwt_info.js
+++ src/config/jwt_info.js
@@ -1,4 +1,4 @@
-import env from "./env_info";
+import env from './env_info';
 
-export const jwt_token_string = env.JWT_KEY || "1234";
+export const jwt_token_string = env.JWT_KEY || '1234';
 export const jwt_token_duration_ms = env.JWT_TOKEN_DURATION_MS || 3600;
