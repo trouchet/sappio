d3ebb8aa647ef6a379f5b5f263ed8b3685f9b992
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-31 16:27:27 -0300

    refac/ move great objects procedural side instead of nested

--- src/core/server.js
+++ src/core/server.js
@@ -3,11 +3,11 @@ import log from '../utils/logger';
 
 const startServer = (port) => {
   const fail_msg = `😸 Application server listening on PORT ${port}`;
-  const success_msg = `😿 Failed to listen on PORT ${port}`;
+  const succ_msg = `😿 Failed to listen on PORT ${port}`;
 
   app.listen(
     port,
-    (err) => err ? log('error', fail_msg) : log('info', success_msg)
+    (err) => err ? log('error', fail_msg) : log('info', succ_msg)
   );
 };
 
