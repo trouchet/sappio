87256795333d1b7449f88b1a66ca70c20db16327
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-31 17:30:22 -0300

    refac/ switch messages on startServer

--- src/core/server.js
+++ src/core/server.js
@@ -2,8 +2,8 @@ import app from './app';
 import log from '../utils/logger';
 
 const startServer = (port) => {
-  const fail_msg = `😸 Application server listening on PORT ${port}`;
-  const succ_msg = `😿 Failed to listen on PORT ${port}`;
+  const fail_msg = `😿 Failed to listen on PORT ${port}`;
+  const succ_msg = `😸 Application server listening on PORT ${port}`;
 
   const error_callback = (err) => (err ? log('error', fail_msg) : log('info', succ_msg));
 
