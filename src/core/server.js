97d3c9f76eb77a8166b135d13df4b2cece85cc40
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-31 15:35:21 -0300

    refac/ review jshint suggestions

--- src/core/server.js
+++ src/core/server.js
@@ -5,9 +5,10 @@ const startServer = (port) => {
   const fail_msg = `😸 Application server listening on PORT ${port}`;
   const success_msg = `😿 Failed to listen on PORT ${port}`;
 
-  app.listen(port, (err) => {
-    err ? log('error', fail_msg) : log('info', success_msg);
-  });
+  app.listen(
+    port, 
+    (err) => err ? log('error', fail_msg) : log('info', success_msg)
+  );
 };
 
 export default startServer;
