8f5037663074f02fcd54dc82a551ae43ef07577f
Bruno Peixoto <brunolnetto@gmail.com>
2022-12-31 18:08:13 -0300

    lint/ add semicolon

--- src/config/app_info.js
+++ src/config/app_info.js
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 
-const files_content = await readFileSync('package.json')
+const files_content = await readFileSync('package.json');
 const pkg = JSON.parse(files_content);
 
 export default pkg;
