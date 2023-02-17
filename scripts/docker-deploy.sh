cf62b8bb0075ce53680c1d13a5babacf67d8e1e5
Bruno Peixoto <brunolnetto@gmail.com>
2023-02-17 18:59:21 -0300

    config/ fix deploy and watch files

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -8,14 +8,14 @@ TO_PORT="$3"
 if [[ $# -eq 0 ]]; then
   echo 'Provide Docker image name, local listen port and output port!'
 
-elif docker ps -a | grep -qw "$1"; then
-  CONTAINER_ID=$(docker ps -a | grep -w "$IMAGE_NAME" | head -n1 | awk "{print $IMAGE_NAME;}");
-
+elif [[ $(docker ps -a | grep -w "$IMAGE_NAME" | wc -l) -eq 1  ]]; then
+  CONTAINER_ID="$(docker ps -a | grep -w "$IMAGE_NAME" | tail -n1 | awk '{print $1}')";
+  
   echo "Container image $IMAGE_NAME with ID $CONTAINER_ID already exists! Watch it below."
-  bash "$(pwd)/scripts/docker-watch.sh" "$CONTAINER_ID"
+  bash "$(pwd)/scripts/docker-watch.sh" $CONTAINER_ID
 
 else
-  echo "No such container image $1 yet"
+  echo "No such container image $IMAGE_NAME yet"
 
   docker build -t "$IMAGE_NAME" .
   docker run -p "$FROM_PORT:$TO_PORT" -d "$IMAGE_NAME"
--- scripts/docker-watch.sh
+++ scripts/docker-watch.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-IMAGE_NAME="$1"
+CONTAINER_ID="$1"
 
 if [[ $# -eq 0 ]];
   then echo 'Container image name not supplied'
-elif docker ps -a | grep -qw "$IMAGE_NAME"; then
-
-  if [[ $(docker ps -a | grep -w "$IMAGE_NAME" | wc -l) -eq 1  ]]; then
-    container_id=$(docker ps -a | grep -w "$IMAGE_NAME" | head -n1 | awk "{print $1;}");
-
-    echo "Container image $IMAGE_NAME with ID $container_id exists!"
-    docker logs -f "$container_id"
+elif docker ps -a | grep -qw "$IMAGE_ID"; then
+  
+  if [[ $(docker ps -a | grep -w "$CONTAINER_ID" | wc -l) -eq 1  ]]; then
+    IMAGE_NAME="$(docker ps -a | grep -w \"$1\" | tail -n1 | awk '{print $2}')";
+    
+    echo "Container image $IMAGE_NAME with ID $CONTAINER_ID exists!"
+    docker logs -f "$CONTAINER_ID"
+  
   else
     echo "We found more than one container with name $IMAGE_NAME."
     echo $(docker ps -a | grep -w "$IMAGE_NAME")
@@ -19,5 +20,5 @@ elif docker ps -a | grep -qw "$IMAGE_NAME"; then
   fi;
 
 else
-  echo "No such container "
+  echo "No such container $IMAGE_NAME"
 fi;
