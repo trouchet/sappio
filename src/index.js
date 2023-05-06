601a500ba17822791f9367d706c5943dd0b7ac08
Bruno Peixoto <brunolnetto@gmail.com>
2023-05-06 19:34:40 -0300

    refac/ use package.json imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // [START app]
 import app from './core/app.js';
 import startServer from './core/server.js';
-import env from './config/env_info.js';
+import env from '#config/env_info.js';
 
 const port = parseInt(env.APP_PORT, 10);
 startServer(app, port);
